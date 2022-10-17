7658b8e04afb4cdea0109955d11f592ba249b56b
moeez <moeezk115@gmail.com>
2022-10-17 11:18:53 +0300

    task uploaded

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 const URL = 'http://localhost:3001/'
 function App() {
   const [tasks, setTasks]= useState([])
+  const [task,setTask] = useState('')
 
   useEffect(() => {
   axios.get(URL)
@@ -21,13 +22,53 @@ function App() {
   return (
     <div >
    <h3>My Tasks</h3>
+   <form>
+    <label>Add new</label>
+    <input value={task} onChange={e => setTask(e.target.value)}/>
+    <button type="submit" onClick={save}>Save</button>
+   </form>
    <ol>
     {tasks.map(task => (
-      <li key={task.id}>{task.description}</li>
+      <li key={task.id}>{task.description} <a href="#" onClick={() => remove(task.id)}>Delete</a></li>
     ))}
    </ol>
     </div>
   );
+
+  function save(){
+    const json = JSON.stringify({description: task})
+    axios.post(URL + 'new',json,{
+      headers: { 
+        'Content-Type': 'application/json'
+      }
+    })
+  
+    .then((response) => {
+  
+      const addedObject = JSON.parse(json)
+      addedObject.id = response.data.id
+  
+      setTasks(tasks => [...tasks, addedObject]);
+      setTask('')
+  
+    }).catch((error) => {
+      alert(error.response.data.error)
+    })
+  }
+  
+  function remove(id) {
+    axios.delete(`${URL}delete/${id}`)
+    .then(()=>{
+      const newListWithoutRemoved = tasks.filter((item)=> item.id !==id)
+      setTasks(newListWithoutRemoved)
+  
+    }).catch (error => {
+      alert(error.response.data.error)
+    })
+  }
 }
+
+
+
 
 export default App;
